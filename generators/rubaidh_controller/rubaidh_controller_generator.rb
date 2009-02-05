4dc8e60d12773b4294f9a19ad03376583ab52dcb
Graeme Mathieson <mathie@woss.name>
2009-02-05 17:41:33 +0000

    Make rubaidh_helper a depdendency, rather than just spitting out the same templates.

--- generators/rubaidh_controller/rubaidh_controller_generator.rb
+++ generators/rubaidh_controller/rubaidh_controller_generator.rb
@@ -4,14 +4,12 @@ class RubaidhControllerGenerator < RubaidhNamedBase
   def manifest
     record do |m|
       # Check for class naming collisions.
-      m.class_collisions "#{class_name}Controller", "#{class_name}Helper"
+      m.class_collisions "#{class_name}Controller"
 
       # Controller, helper, views, and spec directories.
       m.directory File.join('app/controllers', class_path)
-      m.directory File.join('app/helpers', class_path)
       m.directory File.join('app/views', class_path, file_name)
       m.directory File.join('spec/controllers', class_path)
-      m.directory File.join('spec/helpers', class_path)
 
       # Controller spec, class, and helper.
       m.template 'controller.rb',
@@ -19,20 +17,10 @@ class RubaidhControllerGenerator < RubaidhNamedBase
                            class_path,
                            "#{file_name}_controller.rb")
 
-      m.template 'rubaidh_helper:helper.rb',
-                 File.join('app/helpers',
-                           class_path,
-                           "#{file_name}_helper.rb")
-
       m.template 'controller_spec.rb',
                  File.join('spec/controllers',
                            class_path,
                            "#{file_name}_controller_spec.rb")
-
-      m.template 'rubaidh_helper:helper_spec.rb',
-                 File.join('spec/helpers',
-                           class_path,
-                           "#{file_name}_helper_spec.rb")
 
       # View template for each action.
       actions.each do |action|
@@ -40,6 +28,8 @@ class RubaidhControllerGenerator < RubaidhNamedBase
         m.template 'view.html.erb', path,
           :assigns => { :action => action, :path => path }
       end
+
+      m.dependency 'rubaidh_helper', [name] + @args
     end
   end
 
